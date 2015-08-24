5fbd151b057f398ac5d95e723ac15a01355db517
root <root@localhost.localdomain>
2015-08-24 09:57:15 -0400

    use glob.sync rather than the anemic ls provided with shelljs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('shelljs/global');
+// shelljs ls patterns aren't complete, use glob.sync
+var glob = require('glob');
 var fs = require('fs');
 
-var patterns = [ '/var/www/*/web', '/opt/stagecoach/apps/*/current/public' ];
+var patterns = [ '/var/www/*/symfony/web', '/var/www/*/web', '/opt/stagecoach/apps/*/current/public' ];
 
 patterns.forEach(function(pattern) {
-  ls(pattern).forEach(function(folder) {
+  glob.sync(pattern).forEach(function(folder) {
+    console.log(folder);
     fs.writeFileSync(folder + '/robots.txt',
       'User-agent: *\n' +
       'Disallow: /\n'
